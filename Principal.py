df78e0ad9ee4a03fba860d2bb0360230663cca3e
LuanCantalice <luaancantalice@gmail.com>
2016-10-03 21:23:08 -0300

    StringProjeto

---
+++ Principal.py
@@ -0,0 +1,16 @@
+import StringUtil
+
+texto = input(" Texto: ")
+
+#Imprimir tamanho
+#Imprimir Maiusculo
+#Imprimir Minusculo
+#Imprimir a quantidade de vezes que a letra aparece
+
+StringUtil.output(StringUtil.tamanho(texto))
+
+StringUtil.output(StringUtil.maiusculo(texto))
+
+StringUtil.output(StringUtil.minusculo(texto))
+
+StringUtil.output(StringUtil.contador(texto))
